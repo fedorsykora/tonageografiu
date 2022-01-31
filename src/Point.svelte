98bf9423af2769beb45de2f4a9fc80765bf27fce
owo <95430286+fedorsykora@users.noreply.github.com>
2022-01-31 15:07:09 +0100

    Add points module

---
+++ src/Point.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let name: string;
+    export let coords: number[];
+
+    function handleClick(){
+        //TODO
+    }
+</script>
+
+<circle r="5" cx={coords[0]} cy={coords[1]} fill="red"/>
