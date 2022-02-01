18d58f2ad2cedf622d540ccb8e462eb7b468ac93
owo <95430286+fedorsykora@users.noreply.github.com>
2022-02-01 11:37:07 +0100

    Add remaining cities

--- src/Hud.svelte
+++ src/Hud.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-    import {currentCity, mistakes} from './stores';
+    import {currentCity, mistakes, cities, index} from './stores';
 </script>
 
-<p>Mesto: {$currentCity?.name}</p>
-<p>Počet chýb: {$mistakes}</p>
+<div>
+    <p>Mesto: {$currentCity?.name}</p>
+    <p>Počet chýb: {$mistakes}</p>
+    <p>Zostávajúce mestá: {$cities.length - $index}</p>
+</div>
