6f61bb52713f19f7d98095503b9b5c9185fab32f
owo <95430286+fedorsykora@users.noreply.github.com>
2022-02-01 11:21:42 +0100

    Rename Point.svelte to City.svelte

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import MapBackground from './MapBackground.svelte';
-    import Point from './Point.svelte';
+    import City from './City.svelte';
     import {cities} from './stores';
 
     let d:{ [name: string]: number[] } = {
@@ -91,7 +91,7 @@
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 1489.49 739.6" style="enable-background:new 0 0 1489.49 739.6;" xml:space="preserve">
     <MapBackground/>
     {#each Object.entries(d) as [name, coords]}
-        <Point name={name} coords={coords}/>
+        <City name={name} coords={coords}/>
     {/each}
 </svg>
 <a href="https://commons.wikimedia.org/wiki/File:Slovensko_okresy.svg">Liqid1010</a>, CC0, via Wikimedia Commons
